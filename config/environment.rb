248a2d1d6e48f4e0ebe29f3f2ae15d900ee04b30
youniquemag <youniquemag.tech@gmail.com>
2018-02-18 20:19:47 +0100

    Changed development database connectivity settings

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '4.1.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -15,10 +15,17 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
-  # config.gem "bj"
-  # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  # config.gem "sqlite3-ruby", :lib => "sqlite3"
-  # config.gem "aws-s3", :lib => "aws/s3"
+  # config.gem "uglifier"
+    config.gem "rails", :version => '4.1.2', :source => "https://rubygems.org"
+    config.gem "scss-rails", :version => '4.0.3', :source => "https://rubygems.org"
+    config.gem "uglifier", :source => "https://rubygems.org"
+    config.gem "coffee-rails", :version => '4.0.0', :source => "https://rubygems.org"
+    config.gem "jquery-rails", :source => "https://rubygems.org"
+    config.gem "turbolinks", :source => "https://rubygems.org"
+    config.gem "jbuilder", :version => '2.0', :source => "https://rubygems.org"
+    config.gem "annotate", :version => '2.6.5', :source => "https://rubygems.org"
+    config.gem "sdoc", :version => '0.4.0', :group => "doc", :source => "https://rubygems.org"
+    config.gem "spring", :group => "development", :source => "https://rubygems.org"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
